aadf92957fd5d81263a8b2f3b387b4e2b254394a
Anton Kondrashov <kondrashka1313@yandex.ru>
2016-11-22 01:38:20 +0300

    Initial Commit

---
+++ Geoapps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geoapps
+//
+//  Created by Anton Kondrashov on 22/11/2016.
+//  Copyright © 2016 Anton Kondrashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
